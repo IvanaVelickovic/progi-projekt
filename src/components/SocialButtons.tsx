fc9d6347c687a6e1eb08fc7a2bcbd9f66aebff94
IvanaVelickovic <iv56160@fer.hr>
2025-11-08 22:04:20 +0100

    feat: Google and Microsoft auth

--- src/components/SocialButtons.tsx
+++ src/components/SocialButtons.tsx
@@ -1,12 +1,51 @@
 import { Link } from "react-router-dom";
 import GoogleLogo from "../assets/logos/google_logo.png";
 import MicrosoftLogo from "../assets/logos/microsoft_logo.png";
+import { useGoogleLogin } from "@react-oauth/google";
+import { useMsal } from "@azure/msal-react";
 
 interface SocialButtonsProps {
   authType: string;
 }
 
 const SocialButtons = ({ authType }: SocialButtonsProps) => {
+  const { instance } = useMsal();
+
+  const googleLogin = useGoogleLogin({
+    flow: "auth-code",
+    onSuccess: (codeResponse) => {
+      fetch("http://localhost:3000", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ code: codeResponse.code }),
+      });
+    },
+    onError: () => console.log("Google auth failed"),
+  });
+
+  const microsoftLogin = async () => {
+    try {
+      const loginResponse = await instance.loginPopup({
+        scopes: ["User.Read"],
+      });
+
+      const tokenResponse = await instance.acquireTokenSilent({
+        scopes: ["User.Read"],
+        account: loginResponse.account,
+      });
+
+      const accessToken = tokenResponse.accessToken;
+
+      await fetch("http://localhost:3000", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ token: accessToken }),
+      });
+    } catch (e) {
+      console.log("Microsoft login error: ", e);
+    }
+  };
+
   return (
     <>
       <div className="flex items-center justify-center w-3/4 my-2.5">
@@ -15,11 +54,17 @@ const SocialButtons = ({ authType }: SocialButtonsProps) => {
         <div className="border border-blue-dark w-1/2 ml-2"></div>
       </div>
       <div className="flex justify-around w-3/4 text-base">
-        <button className="flex justify-center items-center border-3 border-blue-light rounded-xl bg-white py-1 px-3 text-blue-dark w-5/12 cursor-pointer">
+        <button
+          className="flex justify-center items-center border-3 border-blue-light rounded-xl bg-white py-1 px-3 text-blue-dark w-5/12 cursor-pointer"
+          onClick={googleLogin}
+        >
           <img src={GoogleLogo} className="h-7 w-7 mr-1.5"></img>
           Google
         </button>
-        <button className="flex justify-center items-center border-3 border-blue-light rounded-xl bg-white px-3 text-blue-dark w-5/12 cursor-pointer">
+        <button
+          className="flex justify-center items-center border-3 border-blue-light rounded-xl px-3 py-2 bg-white text-blue-dark cursor-pointer"
+          onClick={microsoftLogin}
+        >
           <img src={MicrosoftLogo} className="h-6 w-6 mr-2"></img>
           Microsoft
         </button>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { MsalProvider } from "@azure/msal-react";
+import { msalInstance } from "./msalConfig";
 
-createRoot(document.getElementById('root')!).render(
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <MsalProvider instance={msalInstance}>
+        <App />
+      </MsalProvider>
+    </GoogleOAuthProvider>
+  </StrictMode>
+);
