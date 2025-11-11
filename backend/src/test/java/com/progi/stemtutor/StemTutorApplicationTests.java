f42be87034ccaa820893deaebf007d8b6d10a82c
unknown <ivanmilos20@gmail.com>
2025-11-11 15:10:11 +0100

    Initialize Spring Boot backend setup

---
+++ backend/src/test/java/com/progi/stemtutor/StemTutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progi.stemtutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StemTutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
