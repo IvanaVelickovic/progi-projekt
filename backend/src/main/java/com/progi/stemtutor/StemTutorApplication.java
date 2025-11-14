6478db960180d21465af4162bdc2a721a26a77a8
ivan-milos <ivanmilos20@gmail.com>
2025-11-14 23:14:30 +0100

    Add database insert

--- backend/src/main/java/com/progi/stemtutor/StemTutorApplication.java
+++ backend/src/main/java/com/progi/stemtutor/StemTutorApplication.java
@@ -1,13 +1,19 @@
 package com.progi.stemtutor;
 
+import com.progi.stemtutor.service.DatabaseInitService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StemTutorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StemTutorApplication.class, args);
+		ApplicationContext context= SpringApplication.run(StemTutorApplication.class, args);
+        DatabaseInitService initService = context.getBean(DatabaseInitService.class);
+
+        // Run your insertions
+        initService.insertSubjects();
 	}
 
 }
