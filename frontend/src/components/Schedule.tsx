cc10d28382c3903e88eae98afac92b4ced6e81e8
IvanaVelickovic <iv56160@fer.hr>
2026-01-17 23:46:49 +0100

    add comments

--- frontend/src/components/Schedule.tsx
+++ frontend/src/components/Schedule.tsx
@@ -1,5 +1,5 @@
 import Calendar from "react-calendar";
-import appointmentsData from "../assets/appointments.json";
+//import appointmentsData from "../assets/appointments.json";
 import { useNavigate } from "react-router-dom";
 import EditSchedule from "./EditSchedule";
 import { useEffect, useState } from "react";
@@ -67,7 +67,7 @@ const Schedule = () => {
     };
 
     fetchAppointments();
-    setAppointments(appointmentsData);
+    //setAppointments(appointmentsData);
   }, []);
 
   const appointmentDates = appointments.map(
