fd19d126864e3b0312aa83534cda05ea9a7efada
IvanaVelickovic <iv56160@fer.hr>
2026-01-22 18:18:31 +0100

    remove redundant line in AddSchedule.tsx

--- frontend/src/pages/AddSchedule.tsx
+++ frontend/src/pages/AddSchedule.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../api";
 import googleLogo from "../assets/logos/google_logo.png";
