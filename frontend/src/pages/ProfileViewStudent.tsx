68ef7b7af13213b6b9139e9723d397a4b88f386c
IvanaVelickovic <iv56160@fer.hr>
2026-01-23 12:28:37 +0100

    add styling changes for profile views

--- frontend/src/pages/ProfileViewStudent.tsx
+++ frontend/src/pages/ProfileViewStudent.tsx
@@ -70,9 +70,7 @@ const ProfileViewStudent = () => {
             {/* LIJEVI PANEL */}
             <div className="w-[260px] bg-[#e9f7f1] rounded-2xl p-6 flex flex-col items-center text-center">
               <div className="h-44 w-44 rounded-full bg-[#9bd7b6] flex items-center justify-center mb-4">
-                Slika
-                <br />
-                učenika
+                <img src="/images/student_bigger.png" className="h-32"></img>
               </div>
               <p className="font-semibold text-blue-dark text-lg">
                 {studentName || "NEMA IMENA"}
