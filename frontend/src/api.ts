95d860d0c8c22d1d20231d046fb377c4b71acc04
IvanaVelickovic <iv56160@fer.hr>
2026-01-23 17:16:57 +0100

    change api.ts url

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+const apiUrl = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: apiUrl,
 });
 
 // Interceptor -> automatski dodaje Authorization header
